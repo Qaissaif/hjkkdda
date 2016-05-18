b8fe1f3c30f8c4b4215d6e5de3d126561e43953d
qaissaif <qais_saif@hotmail.com>
2016-05-18 20:37:53 +0300

    new rade2e

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -5,13 +5,17 @@ class CategoryController < ApplicationController
 
 	def index
 		@category=Category.find(params[:id])
-
-		if @category.id==2
-			# cats=Category.find(id=2).sub_categories
-			# @arts0=cats[0].articles
-			# @arts1=cats[1].articles
-			# @arts2=cats[2].articles
-			# @arts3=cats[3].articles
+		cats=[]
+		if @category.id==2	
+			Category.all.each do |cat|
+				if cat.parent_id!=nil && cats.length < 4
+				cats<<cat
+				end
+			end
+			@cats0=cats[0]
+			@cats1=cats[1]
+			@cats2=cats[2]
+			@cats3=cats[3]
 		else
 			@articles=@category.articles
 		end
