6c2ad80dc96f5e5923c19b61a2444d76d0f3f7c8
Ibrahim Albarghouthi <ibrahimalbarghouthii@hotmail.com>
2016-10-21 19:28:47 +0300

    confs

---
+++ new_deploy.sh
@@ -0,0 +1,19 @@
+cd ~ && git clone git@github.com:Qaissaif/hjkkdda.git hadhada
+sudo apt-get install mysql-server
+sudo service mysql restart
+bundle install
+
+RAILS_ENV=production rake db:create
+RAILS_ENV=production rake db:migrate
+RAILS_ENV=production rake assets:precompile
+
+mkdir -p shared/pids shared/sockets shared/log
+cp /unicorn_hadhada && /etc/init.d/unicorn_hadhada
+sudo update-rc.d unicorn_hadhada defaults
+
+chmod 755 /etc/init.d/unicorn_hadhada
+sudo apt-get install nginx
+sudo cp /nginx-conf /etc/nginx/sites-available/default
+
+sudo service unicorn_hadhada start
+sudo service nginx restart
