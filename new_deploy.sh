99f009479607e789c9dde5a1d30e260d9b234051
Ibrahim Albarghouthi <ibrahimalbarghouthii@hotmail.com>
2016-10-21 20:08:44 +0300

    confs

--- new_deploy.sh
+++ new_deploy.sh
@@ -1,11 +1,11 @@
 cd
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
-git clone git://github.com/sstephenson/rbenv.git .rbenv
+git clone https://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 rbenv install -v 2.1.2
