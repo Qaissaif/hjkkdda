1847b37554435f40bf0fd9fd0aedbeebf599b9ee
qaissaif <qais_saif@hotmail.com>
2017-10-27 16:39:50 +0200

    fix mysql pk issue

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 require File.expand_path('../initializers/abstract_mysql2_adapter', __FILE__)
+require File.expand_path('../initializers/abstract_mysql_adapter', __FILE__)
 
 # Initialize the Rails application.
 Hadhada::Application.initialize!
